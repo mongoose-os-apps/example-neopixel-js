24f650fb086f69c54ca1c75aaf149f40c6023b2a
Бобби <lsm@cesanta.com>
2018-01-01 14:42:53 +0000

    Use timer REPEAT and NOREPEAT flags
    PUBLISHED_FROM=5b4a057606d9e27c5dfc91879bd0c601d3e6e75f

--- fs/init.js
+++ fs/init.js
@@ -4,7 +4,7 @@ load('api_neopixel.js');
 let pin = 5, numPixels = 12, colorOrder = NeoPixel.GRB, i = 0;
 let strip = NeoPixel.create(pin, numPixels, colorOrder);
 
-Timer.set(150, true, function() {
+Timer.set(150, Timer.REPEAT, function() {
   let pixel = i++ % numPixels;
   let r = i % 255, g = i * 2 % 255, b = i * i % 255;
   strip.clear();
